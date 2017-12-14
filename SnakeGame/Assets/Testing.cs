e4b7597572fdb8eb3b4c11d936644c32bbf0413f
Tereschenko Dmitry <dima123-99@mail.ru>
2017-12-14 11:49:55 +0300

    Test

---
+++ SnakeGame/Assets/Testing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour {
+	//public Transform startPosition;
+	public GameObject part;
+	private GameObject newPart;
+	private Vector3 UpdatePoint;
+	private Vector3 dir;
+	private float speed = 2f;
+	// Use this for initialization
+	void Start () {
+		UpdatePoint = new Vector3(transform.position.x, transform.position.y, transform.position.z - transform.localScale.y);
+		dir = new Vector3(0f, 0f, -transform.localScale.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Mathf.Abs(Vector3.Distance(transform.position, UpdatePoint)) > speed * Time.deltaTime)
+		{
+			transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+		}else
+		{
+			newPart = Instantiate(part, transform.position, transform.rotation);
+			speed = 0f;
+		}
+	}
+}
---
+++ SnakeGame/Assets/Testing2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing2 : MonoBehaviour {
+	private float speed = 1f;
+	private Vector3 deltaPos;
+	private Vector3 deltaScale;
+	private Vector3 ScaleStart;
+	// Use this for initialization
+	void Start () {
+		ScaleStart = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		deltaPos = new Vector3(0f, 0f, -speed * Time.deltaTime);
+		deltaScale = new Vector3(0f, speed * Time.deltaTime, 0f);
+		transform.position += deltaPos;
+		transform.localScale += deltaScale;
+		transform.GetChild(0).transform.localScale = new Vector3(transform.GetChild(0).transform.localScale.x, (ScaleStart.y / transform.localScale.y), transform.GetChild(0).transform.localScale.z);
+		transform.GetChild(1).transform.localScale = new Vector3(transform.GetChild(1).transform.localScale.x, (ScaleStart.y / transform.localScale.y), transform.GetChild(1).transform.localScale.z);
+
+	}
+}
